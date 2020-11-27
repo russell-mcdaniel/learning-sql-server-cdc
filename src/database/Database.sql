ba4dbbdcd85518341e6cd774a5a29e0b514707fa
Russell McDaniel <russell.mcdaniel@siliconesis.com>
2020-11-27 12:50:20 -0800

    Update schema, use CDC filegroup, and support host volume

--- src/database/Database.sql
+++ src/database/Database.sql
@@ -1,3 +1,5 @@
+:on error exit
+
 :setvar DatabaseName "MedicationTracker"
 
 -- ================================================================================
@@ -16,10 +18,47 @@ END;
 GO
 
 CREATE DATABASE [$(DatabaseName)]
-ON
+ON PRIMARY
 (
     NAME        = $(DatabaseName),
-    FILENAME    = '/var/opt/mssql/data/$(DatabaseName).mdf',
+--  FILENAME    = '/var/opt/mssql/data/$(DatabaseName).mdf',
+    FILENAME    = '/home/mssql/data/$(DatabaseName).mdf',
+	SIZE		= 64 MB,
+	MAXSIZE		= UNLIMITED,
+	FILEGROWTH	= 64 MB
+),
+FILEGROUP CoreData
+(
+    NAME        = $(DatabaseName)CoreData,
+--  FILENAME    = '/var/opt/mssql/data/$(DatabaseName)CoreData.mdf',
+    FILENAME    = '/home/mssql/data/$(DatabaseName)CoreData.ndf',
+	SIZE		= 64 MB,
+	MAXSIZE		= UNLIMITED,
+	FILEGROWTH	= 64 MB
+),
+FILEGROUP CoreCdc
+(
+    NAME        = $(DatabaseName)CoreCdc,
+--  FILENAME    = '/var/opt/mssql/data/$(DatabaseName)CoreCdc.ndf',
+    FILENAME    = '/home/mssql/data/$(DatabaseName)CoreCdc.ndf',
+	SIZE		= 64 MB,
+	MAXSIZE		= UNLIMITED,
+	FILEGROWTH	= 64 MB
+),
+FILEGROUP TransactionData
+(
+    NAME        = $(DatabaseName)TransactionData,
+--  FILENAME    = '/var/opt/mssql/data/$(DatabaseName)TransactionData.ndf',
+    FILENAME    = '/home/mssql/data/$(DatabaseName)TransactionData.ndf',
+	SIZE		= 64 MB,
+	MAXSIZE		= UNLIMITED,
+	FILEGROWTH	= 64 MB
+),
+FILEGROUP TransactionCdc
+(
+    NAME        = $(DatabaseName)TransactionCdc,
+--  FILENAME    = '/var/opt/mssql/data/$(DatabaseName)TransactionCdc.ndf',
+    FILENAME    = '/home/mssql/data/$(DatabaseName)TransactionCdc.ndf',
 	SIZE		= 64 MB,
 	MAXSIZE		= UNLIMITED,
 	FILEGROWTH	= 64 MB
@@ -27,7 +66,8 @@ ON
 LOG ON
 (
     NAME        = $(DatabaseName)Log,
-    FILENAME    = '/var/opt/mssql/data/$(DatabaseName).ldf',
+--  FILENAME    = '/var/opt/mssql/data/$(DatabaseName).ldf',
+    FILENAME    = '/home/mssql/data/$(DatabaseName).ldf',
 	SIZE		= 64 MB,
 	MAXSIZE		= UNLIMITED,
 	FILEGROWTH	= 64 MB
@@ -72,20 +112,20 @@ ALTER TABLE dbo.Company
     ADD CONSTRAINT pk_Company
     PRIMARY KEY CLUSTERED (CompanyKey)
     WITH FILLFACTOR = 80
-    ON [PRIMARY];
+    ON CoreData;
 GO
 
 ALTER TABLE dbo.Company
     ADD CONSTRAINT uk_Company_CompanyName
     UNIQUE (CompanyName)
     WITH FILLFACTOR = 80
-    ON [PRIMARY];
+    ON CoreData;
 GO
 
-CREATE NONCLUSTERED INDEX ix_Company_1
+CREATE NONCLUSTERED INDEX ix_Company_Rover
     ON dbo.Company (Rover)
-    WITH FILLFACTOR = 80
-    ON [PRIMARY];
+    WITH FILLFACTOR = 100
+    ON CoreData;
 GO
 
 EXECUTE sys.sp_cdc_enable_table
@@ -96,7 +136,59 @@ EXECUTE sys.sp_cdc_enable_table
     @role_name = NULL,              -- No gating role.
     @index_name = NULL,
     @captured_column_list = NULL,
-    @filegroup_name = N'PRIMARY',
+    @filegroup_name = N'CoreCdc',
+    @allow_partition_switch = 1;
+GO
+
+-- --------------------------------------------------------------------------------
+-- UserAccount
+-- --------------------------------------------------------------------------------
+CREATE TABLE dbo.UserAccount
+(
+    CompanyKey                  uniqueidentifier    NOT NULL,
+    UserAccountKey              uniqueidentifier    NOT NULL,
+    Email                       nvarchar(100)       NOT NULL,
+    DisplayName                 nvarchar(100)       NOT NULL,
+    CreatedAt                   datetime2(7)        NOT NULL,
+    Rover                       rowversion          NOT NULL
+);
+GO
+
+ALTER TABLE dbo.UserAccount
+    ADD CONSTRAINT pk_UserAccount
+    PRIMARY KEY CLUSTERED (CompanyKey, UserAccountKey)
+    WITH FILLFACTOR = 80
+    ON CoreData;
+GO
+
+ALTER TABLE dbo.UserAccount
+    ADD CONSTRAINT uk_UserAccount_CompanyKeyEmail
+    UNIQUE (CompanyKey, Email)
+    WITH FILLFACTOR = 80
+    ON CoreData;
+GO
+
+ALTER TABLE dbo.UserAccount
+    ADD CONSTRAINT fk_UserAccount_CompanyKey_Company
+    FOREIGN KEY (CompanyKey)
+    REFERENCES dbo.Company (CompanyKey);
+GO
+
+CREATE NONCLUSTERED INDEX ix_UserAccount_Rover
+    ON dbo.UserAccount (Rover)
+    WITH FILLFACTOR = 100
+    ON CoreData;
+GO
+
+EXECUTE sys.sp_cdc_enable_table
+    @source_schema = N'dbo',
+    @source_name = N'UserAccount',
+--  @capture_instance = N'',        -- Use default name for demo.
+    @supports_net_changes = 0,      -- No functions for net changes.
+    @role_name = NULL,              -- No gating role.
+    @index_name = NULL,
+    @captured_column_list = NULL,
+    @filegroup_name = N'CoreCdc',
     @allow_partition_switch = 1;
 GO
 
@@ -117,7 +209,14 @@ ALTER TABLE dbo.Facility
     ADD CONSTRAINT pk_Facility
     PRIMARY KEY CLUSTERED (CompanyKey, FacilityKey)
     WITH FILLFACTOR = 80
-    ON [PRIMARY];
+    ON CoreData;
+GO
+
+ALTER TABLE dbo.Facility
+    ADD CONSTRAINT uk_Facility_CompanyKeyFacilityName
+    UNIQUE (CompanyKey, FacilityName)
+    WITH FILLFACTOR = 80
+    ON CoreData;
 GO
 
 ALTER TABLE dbo.Facility
@@ -126,10 +225,10 @@ ALTER TABLE dbo.Facility
     REFERENCES dbo.Company (CompanyKey);
 GO
 
-CREATE NONCLUSTERED INDEX ix_Facility_1
+CREATE NONCLUSTERED INDEX ix_Facility_Rover
     ON dbo.Facility (Rover)
-    WITH FILLFACTOR = 80
-    ON [PRIMARY];
+    WITH FILLFACTOR = 100
+    ON CoreData;
 GO
 
 EXECUTE sys.sp_cdc_enable_table
@@ -140,7 +239,7 @@ EXECUTE sys.sp_cdc_enable_table
     @role_name = NULL,              -- No gating role.
     @index_name = NULL,
     @captured_column_list = NULL,
-    @filegroup_name = N'PRIMARY',
+    @filegroup_name = N'CoreCdc',
     @allow_partition_switch = 1;
 GO
 
@@ -149,6 +248,12 @@ GO
 -- --------------------------------------------------------------------------------
 CREATE TABLE dbo.Item
 (
+    CompanyKey                  uniqueidentifier    NOT NULL,
+    ItemKey                     uniqueidentifier    NOT NULL,
+    DisplayName                 nvarchar(100)       NOT NULL,
+    FacilityKey                 uniqueidentifier    NOT NULL,
+    CreatedAt                   datetime2(7)        NOT NULL,
+    Rover                       rowversion          NOT NULL
 );
 GO
 
@@ -156,5 +261,59 @@ ALTER TABLE dbo.Item
     ADD CONSTRAINT pk_Item
     PRIMARY KEY CLUSTERED (CompanyKey, ItemKey)
     WITH FILLFACTOR = 80
-    ON [PRIMARY];
+    ON CoreData;
+GO
+
+ALTER TABLE dbo.Item
+    ADD CONSTRAINT uk_Item_FacilityKeyItemKey
+    UNIQUE (FacilityKey, ItemKey)
+    WITH FILLFACTOR = 80
+    ON CoreData;
+GO
+
+ALTER TABLE dbo.Item
+    ADD CONSTRAINT fk_Item_CompanyKey_Company
+    FOREIGN KEY (CompanyKey)
+    REFERENCES dbo.Company (CompanyKey);
+GO
+
+ALTER TABLE dbo.Item
+    ADD CONSTRAINT fk_Item_FacilityKey_Facility
+    FOREIGN KEY (CompanyKey, FacilityKey)
+    REFERENCES dbo.Facility (CompanyKey, FacilityKey);
+GO
+
+CREATE NONCLUSTERED INDEX ix_Item_FacilityKey
+    ON dbo.Item (FacilityKey)
+    WITH FILLFACTOR = 80
+    ON CoreData;
+GO
+
+CREATE NONCLUSTERED INDEX ix_Item_Rover
+    ON dbo.Item (Rover)
+    WITH FILLFACTOR = 100
+    ON CoreData;
+GO
+
+EXECUTE sys.sp_cdc_enable_table
+    @source_schema = N'dbo',
+    @source_name = N'Item',
+--  @capture_instance = N'',        -- Use default name for demo.
+    @supports_net_changes = 0,      -- No functions for net changes.
+    @role_name = NULL,              -- No gating role.
+    @index_name = NULL,
+    @captured_column_list = NULL,
+    @filegroup_name = N'CoreCdc',
+    @allow_partition_switch = 1;
+GO
+
+
+-- ================================================================================
+-- DATABASE CONFIGURATION
+-- ================================================================================
+
+-- Change Data Capture Retention Period
+EXECUTE sys.sp_cdc_change_job
+    @job_type = N'cleanup',
+    @retention = 1440;              -- 1 day.
 GO
